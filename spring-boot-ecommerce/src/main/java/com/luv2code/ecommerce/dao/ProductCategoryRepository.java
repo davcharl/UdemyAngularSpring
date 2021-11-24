fb137831c6c91b61aebac9b0a2b151fc4872cacf
davcharl <davidc.2107@gmail.com>
2021-11-24 15:50:21 +0000

    Added CrossOrigin support to spring boot backend. Frist version of angular app completed

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv2code.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 // This overrides defaults.
 // "productCategory" will be the name of the JSON entry (that, i guess, binds to the entity of the same name).
 // "path" will be the endpoint /product-category (otherwise Jpa will just append 's'/pluralises to the entity name: /productCategories, which is not what i want to be seen)
